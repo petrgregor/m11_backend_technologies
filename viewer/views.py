9cab2ba163b8a727e925f5bc110d4719aa0b7183
Petr Gregor <petr.gregor@email.cz>
2024-08-27 13:03:47 +0200

    Hello world.

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def hello(request):
+    return HttpResponse("Hello, world!")
+
+
+def hello2(request, s):
+    return HttpResponse(f"Hello, {s} world!")
